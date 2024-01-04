e90f3a6c2991a40dafa8b0cd9efc3c840be7b5ad
aselemidivine <aselemidivine@gmail.com>
2024-01-04 11:49:36 +0100

    pull from divine branch and npm install

--- components/Nationality.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import Droplet from "../components/SAccount";
-
-const Nationality = () => {
-  const [selected, setSelected] = useState("Choose");
-  return <Droplet selected={selected} setSelected={setSelected} />;
-};
-
-export default Nationality;
