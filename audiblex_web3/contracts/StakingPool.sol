2d32a50b0d6c0df4914e3511004ef06d67ceaa7f
Sheys <95696361+Sheys11@users.noreply.github.com>
2024-01-08 04:24:48 -0800

    sheys/ADB-003 | Chore: Added shares & price conversion implementation

--- audiblex_web3/contracts/StakingPool.sol
+++ audiblex_web3/contracts/StakingPool.sol
@@ -5,24 +5,26 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract StakingPool is ERC20 {
-    address public tokenAddress;
+    IERC20 public tokenAddress;
 
     uint256 constant MAX_PERCENTAGE = 10000;
     
-    constructor() ERC20("Audiblex-V1", "Audi-V1") {
+    constructor(address _tokenAddress) ERC20("Audiblex-V1", "Audi-V1") {
+        tokenAddress = IERC20(_tokenAddress);;
     }
 
     function stake(uint256 _tokenAmount, address _content, uint256 _tokenId) public payable returns(uint256) {
         if(getReserve() == 0){
+            token.transferFrom(msg.sender, address(this), _tokenAmount);
         // LP token issued is the exact amount of ethers deposited
-        uint256 liquidity = address(this).balance;
+        uint256 liquidity = _tokenAmount;
         _mint(msg.sender, liquidity);
         return liquidity;
         } else {
-            uint256 ethReserve = address(this).balance - msg.value; //deducts incoming msg.value from reserve
-
-            // LP token issued is proportional to amount of ethers deposited
-            uint256 liquidity = (totalSupply() * msg.value) / ethReserve;
+            token.transferFrom(msg.sender, address(this), _tokenAmount);
+            uint256 tokenReserve = getReserve();
+            // LP token issued is proportional to amount of tokens deposited
+            uint256 liquidity = (totalSupply() * _tokenAmount) / tokenReserve;
             _mint(msg.sender, liquidity);
             return liquidity;
         }
@@ -39,17 +41,21 @@ contract StakingPool is ERC20 {
         uint256 fee = _amount - threshold;
         
 
-        uint256 ethAmount = (address(this).balance * threshold) / totalSupply();
+        uint256 tokenAmount = (getReserve() * threshold) / totalSupply();
 
         _burn(msg.sender, _amount);
-        payable(msg.sender).transfer(ethAmount);
+        token.transfer(tokenAmount);
         
         //Transfer fees to feeManager
-        payable(router).transfer(fee);
-        return (ethAmount);
+        payable(feeManager).transfer(fee);
+        return (tokenAmount);
     }
 
     function getReserve() public view returns(uint256) {
-        return IERC20(tokenAddress).balanceOf(address(this));
+        return token.balanceOf(address(this));
+    }
+
+    function getValue() public view returns(uint256) {
+        return getReserve().getConversionRate();
     }
 }
