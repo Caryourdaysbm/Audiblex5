7af94da62dab33082cdb852dfaeb621414203f22
Oluwakayode Samuel Adeyemi <65360089+Caryourdaysbm@users.noreply.github.com>
2024-01-09 23:38:18 +0100

    Update coming-soon.jsx

--- pages/coming-soon.jsx
+++ pages/coming-soon.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ComingSoon from "../components/landing/ComingSoon";
-import GNavbar from "../components/GeneralNav";
+// import ComingSoon from "../components/landing/ComingSoon";
+// import GNavbar from "../components/GeneralNav";
 const ComingSoonView = () =>{
 return (
     <>
